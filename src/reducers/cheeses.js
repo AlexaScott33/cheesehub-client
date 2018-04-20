c457d2a2d8c9ef6df4d4206b816fdb9115ebaecc
Alexa Scott <arscott4792@gmail.com>
2018-04-19 19:25:37 -0700

    created reducer

---
+++ src/reducers/cheeses.js
@@ -0,0 +1,33 @@
+import {
+    FETCH_CHEESES_REQUEST, 
+    FETCH_CHEESES_SUCCESS, 
+    FETCH_CHEESES_ERROR
+} from '../actions';
+
+const initialState = {
+    cheeses: [],
+    loading: false,
+    error: null
+}
+
+export function cheesesReducer(state=initialState, action) {
+    if (action.type === FETCH_CHEESES_REQUEST) {
+        return Object.assign({}, state, {
+            loading: true
+        });
+    }
+    else if (action.type === FETCH_CHEESES_SUCCESS) {
+        return Object.assign({}, state, {
+            cheeses: action.cheeses,
+            loading: false,
+            error: null
+        });
+    }
+    else if (action.type === FETCH_CHEESES_ERROR) {
+        return Object.assign({}, state, {
+            loading: false,
+            error: action.error
+        });
+    }
+    return state;
+} 
